03e4af807bb30115f8949bf60f0ee7e757481662
NovaFlicker <161892101+AbidAbdullah199@users.noreply.github.com>
2024-09-04 13:19:03 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Oᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>D Lᴜғғʏ</a>\n○ Cʜᴀɴɴᴇʟ : <a href='https://t.me/AnimeQuestX'>Aɴɪᴍᴇ Qᴜᴇsᴛ</a>○ Oɴɢᴏɪɴɢ Cʜᴀɴɴᴇʟ : <a href='https://OngoingAnimeQuest'>Jᴏɪɴ Nᴏᴡ</a>\n○ Dɪsᴄᴜssɪᴛᴏɴ Cʜᴀɴɴᴇʟ : <a href='https://t.me/+r-x-wA4JT5gxZjVl'>Jᴏɪɴ Nᴏᴡ</a>\n○ Mᴀɴɢᴀ Cʜᴀɴɴᴇʟ : <a>href'https://t.me/AnimeQuestManga'>Jᴏɪɴ Nᴏᴡ</a>\n○ Hɪɴᴅɪ Cʜᴀɴɴᴇʟ : <a href'https://t.me/AnimeQuestHindi'>Join Now</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
