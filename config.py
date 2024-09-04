576ca3e60a9ea7a3fba1929ddc7d0cab2aa1e30f
NovaFlicker <161892101+AbidAbdullah199@users.noreply.github.com>
2024-09-04 12:19:08 +0600

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 # check my discription to help by using my refer link of shareus.io
 # 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "https://vnshortener.com")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "vnshortener.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "fd3947a82f375d52856ebe596f0085fd986a8469")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 600)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
